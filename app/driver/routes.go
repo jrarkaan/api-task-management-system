2c011d74eebc0cc0e511b5be5f18c2ea370bef99
jrarkaan07 <rakajanitraa@hotmail.com>
2026-04-26 21:37:54 +0700

    fix(routes): fixing routes when 404

--- app/driver/routes.go
+++ app/driver/routes.go
@@ -6,9 +6,19 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+
+	"api-task-management-system/pkg/apiresponse"
 )
 
 func (d *Driver) RegisterRoutes() {
+	d.router.NoRoute(func(ctx *gin.Context) {
+		if ctx.Request.Method == http.MethodOptions {
+			ctx.Status(http.StatusNoContent)
+			return
+		}
+		apiresponse.NotFound(ctx, nil, "route not found")
+	})
+
 	d.router.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"message": "ok"})
 	})
